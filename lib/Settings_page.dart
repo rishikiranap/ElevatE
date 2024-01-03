0acb7d03c07fffdcf7399d5ca60da8702e634680
rishikiranap <86961915+rishikiranap@users.noreply.github.com>
2024-01-03 14:02:59 +0100

    Some fixes

--- lib/Settings_page.dart
+++ lib/Settings_page.dart
@@ -26,15 +26,16 @@ class _SettingsPageState extends State<SettingsPage> {
         backgroundColor: Colors.black,
         title: Row(
           children: [
-            const Padding(
-              padding: EdgeInsets.all(8.0),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
               child: Text('Settings'),
             ),
-            const Spacer(),
+            Spacer(),
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: Image.asset(
-                'assets/img.png', // Replace with the correct path to your dummy logo
+                'assets/img.png',
+                // Replace with the correct path to your dummy logo
                 width: 65,
                 height: 65,
               ),
@@ -48,7 +49,7 @@ class _SettingsPageState extends State<SettingsPage> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             SwitchListTile(
-              title: const Text('Experience Mode'),
+              title: Text('Experience Mode'),
               value: isDarkModeEnabled,
               onChanged: (value) {
                 setState(() {
@@ -56,26 +57,29 @@ class _SettingsPageState extends State<SettingsPage> {
                   Provider.of<AppSettings>(context, listen: false)
                       .toggleTheme();
                 });
-                // Add logic to change the theme based on the switch state
-                // You can use a theme provider or directly update the theme here
               },
             ),
             ListTile(
-              title: const Text('Number of Quotes to Display'),
+              title: Text('Number of Quotes to Display'),
               subtitle: Text('Current: ${appSettings.numberOfQuotes}'),
               trailing: IconButton(
-                icon: const Icon(Icons.edit),
+                icon: Icon(Icons.edit),
                 onPressed: () {
-                  // Add logic to show a dialog or navigate to a page for editing the number of quotes
-                  // For simplicity, a dialog is used here
                   _showNumberOfQuotesDialog(context, appSettings);
                 },
               ),
             ),
             ListTile(
-              title: const Text('About'),
+              title: Text('About'),
               onTap: () {
                 _showAboutDialog(context);
+              },
+            ),
+            // New ListTile for Ethical and Privacy Concerns
+            ListTile(
+              title: Text('Ethical and Privacy Concerns'),
+              onTap: () {
+                _showEthicalPrivacyDialog(context);
               },
             ),
           ],
@@ -89,19 +93,17 @@ class _SettingsPageState extends State<SettingsPage> {
               label: "settings", icon: Icon(Icons.settings)),
         ],
         currentIndex: _currentIndex,
-
         onTap: (int index) {
           setState(() {
             _currentIndex = index;
           });
           if (index == 0) {
-
             Navigator.push(
               context,
               MaterialPageRoute(
-                  builder: (context) => FirstPage(quotes: globalQuotes,)),
+                  builder: (context) => FirstPage(quotes: globalQuotes)),
             );
-            
+            print(globalQuotes);
           }
           if (index == 1) {
             Navigator.push(
@@ -119,25 +121,27 @@ class _SettingsPageState extends State<SettingsPage> {
       ),
     );
   }
+
   void _showAboutDialog(BuildContext context) {
     showDialog(
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
-          contentPadding: const EdgeInsets.all(16.0),
-          title: const Center(
+          contentPadding: EdgeInsets.all(16.0),
+          title: Center(
             child: Text('About the App'),
           ),
           content: Column(
             mainAxisSize: MainAxisSize.min,
             children: [
               Image.asset(
-                'assets/img.png', // Replace with the correct path to your app logo
+                'assets/img.png',
+                // Replace with the correct path to your app logo
                 width: 100,
                 height: 100,
               ),
-              const SizedBox(height: 16),
-              const Text('ElevatE V1.0'),
+              SizedBox(height: 16),
+              Text('ElevatE V1.0'),
               // Add more details about your app
             ],
           ),
@@ -146,7 +150,7 @@ class _SettingsPageState extends State<SettingsPage> {
               onPressed: () {
                 Navigator.of(context).pop();
               },
-              child: const Text('Close'),
+              child: Text('Close'),
             ),
           ],
         );
@@ -154,22 +158,21 @@ class _SettingsPageState extends State<SettingsPage> {
     );
   }
 
-  void _showNumberOfQuotesDialog(
-      BuildContext context, AppSettings appSettings) {
+  void _showNumberOfQuotesDialog(BuildContext context,
+      AppSettings appSettings) {
     showDialog(
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
-          title: const Text('Edit Number of Quotes'),
+          title: Text('Edit Number of Quotes'),
           content: Column(
             mainAxisSize: MainAxisSize.min,
             children: [
-              const Text('Enter the number of quotes to display:'),
-              const SizedBox(height: 16),
+              Text('Enter the number of quotes to display:'),
+              SizedBox(height: 16),
               TextField(
                 keyboardType: TextInputType.number,
                 onChanged: (value) {
-                  // Validate and update the number of quotes
                   if (value.isNotEmpty) {
                     appSettings.updateNumberOfQuotes(int.parse(value));
                   }
@@ -182,14 +185,99 @@ class _SettingsPageState extends State<SettingsPage> {
               onPressed: () {
                 Navigator.of(context).pop();
               },
-              child: const Text('Cancel'),
+              child: Text('Cancel'),
             ),
             TextButton(
               onPressed: () {
-                // Add logic to save the new number of quotes
                 Navigator.of(context).pop();
               },
-              child: const Text('Save'),
+              child: Text('Save'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _showEthicalPrivacyDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Ethical and Privacy Concerns'),
+          content: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                SizedBox(height: 12.0),
+                Text(
+                  'We prioritize your privacy and are committed to the ethical use of data. Below are key aspects related to privacy and ethics in our application:',
+                  style: TextStyle(fontSize: 16.0),
+                ),
+                SizedBox(height: 16.0),
+                ListTile(
+                  title: Text(
+                    '1. Data Collection:',
+                    style: TextStyle(fontSize: 14.0, fontWeight: FontWeight.bold),
+                  ),
+                  subtitle: Text(
+                    'We collect minimal data necessary for the functionality of the app. This may include user preferences and interactions within the app.',
+                  ),
+                ),
+                SizedBox(height: 8.0),
+                ListTile(
+                  title: Text(
+                    '2. User Input:',
+                    style: TextStyle(fontSize: 14.0, fontWeight: FontWeight.bold),
+                  ),
+                  subtitle: Text(
+                    'Any information provided by users, such as mood selections or custom settings, is stored securely on your device. We do not transmit this data to external servers without your explicit consent. However, the mood selection you choose is sent to an external API for generating motivational quotes.',
+                  ),
+                ),
+                SizedBox(height: 8.0),
+                ListTile(
+                  title: Text(
+                    '3. Data Security:',
+                    style: TextStyle(fontSize: 14.0, fontWeight: FontWeight.bold),
+                  ),
+                  subtitle: Text(
+                    'Your data is stored securely and is inaccessible to unauthorized parties. We use industry-standards to protect your information.',
+                  ),
+                ),
+                SizedBox(height: 8.0),
+                ListTile(
+                  title: Text(
+                    '4. Third-Party Services:',
+                    style: TextStyle(fontSize: 14.0, fontWeight: FontWeight.bold),
+                  ),
+                  subtitle: Text(
+                    'The app may use third-party services for certain features. Your input data is used carefully and with utmost security, and privacy are ensured.',
+                  ),
+                ),
+                SizedBox(height: 8.0),
+                ListTile(
+                  title: Text(
+                    '5. Updates and Notifications:',
+                    style: TextStyle(fontSize: 14.0, fontWeight: FontWeight.bold),
+                  ),
+                  subtitle: Text(
+                    'Periodic updates may be released to enhance app features. You will be notified about updates, and your data will not be used for any purpose other than improving your experience.',
+                  ),
+                ),
+                SizedBox(height: 16.0),
+                Text(
+                  'If you have any questions or concerns regarding privacy and ethics, please contact our support team at support@elevate.com.',
+                  style: TextStyle(fontSize: 16.0),
+                ),
+              ],
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('Close'),
             ),
           ],
         );
